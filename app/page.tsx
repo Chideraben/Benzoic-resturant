5413c0257beb77b75e33ae861ebc881e5210b3d5
Chideraben <chiderabenedict04@gmail.com>
2025-03-28 14:25:35 +0100

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import { Navbar } from "../components/nav";
-import { Hero } from "@/components/hero";
-import { Menu } from "./mealplan/page";
+import Navbar  from "../components/nav";
+import  Hero  from "@/components/hero";
+import Menu  from "./mealplan/page";
 export default function Home() {
   return (
     <div>
