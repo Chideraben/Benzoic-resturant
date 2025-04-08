da9cb9ba2f4a45cd48264843bed1fb6cdafdf8ec
Chideraben <chiderabenedict04@gmail.com>
2025-04-08 11:03:53 +0100

    add

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -5,7 +5,7 @@ const Blog =()=>{
         <div className="p-10 flex flex-col justify-center items-center gap-8">
             <div className="max-w-[400px]">
                 <section className="text-[#030303] font-[600] text-3xl text-center">Blog Our Latest News</section>
-                <h1 className="text-[#0A244E] font-[400] text-sm text-center ">
+                <h1 className="text-[#0A244E] font-[400] text-xs text-center ">
                     Have you ever browsed food blogs like mine 
                     and wondered how to start a food blog of your very own? 
                 </h1>
